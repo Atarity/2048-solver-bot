2f0f27d3a3a37b4ca463d435b20a63b4b050f39b
Atarity <atarity@gmail.com>
2014-04-27 01:02:07 +0400

    Basic rules added

--- 2048-solver-bot.py
+++ 2048-solver-bot.py
@@ -2,23 +2,65 @@ import os, time, re, sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-chromedriver = "/Users/user/Downloads/chromedriver"
+chromedriver = "/Users/user/Downloads/chromedriver"		#tricky part depends on bug in Python/Selenium, SO it
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 
 driver.get("http://gabrielecirulli.github.io/2048/")
 assert "2048" in driver.title
 
-def growth(garden):
-	bed = [[0 for _ in range (4)] for _ in range (4)]
-	for i in garden:
-		mes = re.findall(r"\d+", str(i.get_attribute("class")))
-		bed[int(mes[2])-1][int(mes[1])-1] = int(mes[0])
+Garden = [[0 for _ in range (4)] for _ in range (4)]	#global matrix for storing tiles state
+
+#find all page elements with class-name "tile" and parse it class-names, make matrix from this data and show it 
+def growth(gardenG):
+	global Garden
+	Garden = [[0 for _ in range (4)] for _ in range (4)]
+	print "Garden:"
+	for i in gardenG:
+		mes = re.findall(r"\d+", str(i.get_attribute("class")))		#take only digits
+		Garden[int(mes[2])-1][int(mes[1])-1] = int(mes[0])		#put it to the 2D matrix
 		#print mes
-	for row in bed:
+	for row in Garden:
 	    for val in row:
 	        print '{:4}'.format(val),
 	    print
+
+def zeroRemove(lineZ):
+	for k in range(0, 3):
+		for i in range(0, 3):
+			if lineZ[i] == 0 and lineZ[i+1] != 0:
+				x = lineZ[i+1]
+				lineZ[i] = x
+				lineZ[i+1] = 0
+	return lineZ
+
+def powerPerform(lineP):
+	for i in range(0, 3):
+		if lineP[i] == lineP[i+1]:
+			y = lineP[i]*2
+			lineP[i] = y
+			lineP[i+1] = 0
+	return lineP
+
+def lineAction(lineL):
+	if len(set(lineL)) == 1:	#if all elements is identical
+		x = lineL[0]*2
+		lineL = [x, x, 0, 0]
+	else:
+		ineL = zeroRemove(lineL)
+		lineL = powerPerform(lineL)
+		ineL = zeroRemove(lineL)
+	return lineL
+
+def turnEmul(gardenT):
+	for i in range(0, 4):
+		mesT = [gardenT[3][i], gardenT[2][i], gardenT[1][i], gardenT[0][i]]
+		print lineAction(mesT)
+	"""print "Emulated:"
+	for row in mesT:
+	    for val in row:
+	        print '{:4}'.format(val),
+	    print"""
 
 """element = driver.find_element_by_tag_name("body")
 element.send_keys(Keys.ARROW_DOWN)
@@ -31,10 +73,15 @@ element.send_keys(Keys.ARROW_RIGHT)"""
 while True:
 	grid = ["grid", "growth", "g"]
 	quit = ["stop", "exit", "quit", "q"]
+	action = ["action", "act"]
 	response = raw_input()
 	if response in grid:
-		garden = driver.find_elements_by_class_name("tile")
-		growth(garden)
+		seeds = driver.find_elements_by_class_name("tile")
+		growth(seeds)
+	elif response in action:
+		seeds = driver.find_elements_by_class_name("tile")
+		growth(seeds)
+		turnEmul(Garden)
 	elif response in quit:
 		time.sleep(0.1)
 		driver.close()
